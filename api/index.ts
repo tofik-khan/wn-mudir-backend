794d38f25d72a17f4f89ae536d956bb2239034ad
Tofik Khan <tofik.khan@mkausa.org>
2025-05-13 16:16:19 -0700

    Fix: Module Exports for api

--- api/index.ts
+++ api/index.ts
@@ -7,3 +7,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
+
+module.exports = app;
