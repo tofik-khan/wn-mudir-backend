f51b913c87eebbbd68e715e537deb2612667c1f0
Tofik Khan <tofik.khan@mkausa.org>
2025-05-13 16:14:59 -0700

    API structure setup

---
+++ api/index.ts
@@ -0,0 +1,9 @@
+require('dotenv').config()
+import cors from "cors";
+import express from "express";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
