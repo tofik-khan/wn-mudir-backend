dece1921efb38d60635f1f4fbeaa95b294bdcba0
Tofik Khan <tofik.khan@mkausa.org>
2025-05-19 17:15:47 -0700

    Complete updateProjects route

--- api/waqfeardhi/projects.ts
+++ api/waqfeardhi/projects.ts
@@ -1,6 +1,7 @@
 require("dotenv").config();
 import dayjs from "dayjs";
 import { MongoClient, ObjectId } from "mongodb";
+import { createUpdatePayload } from "../../utils/payload";
 
 const client = new MongoClient(
   `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/`
@@ -36,6 +37,36 @@ export const createProject = async (req, res) => {
         editedBy: req.body.createdBy,
         editedAt: dayjs().format(),
       });
+    res.send({ status: "success", data: result });
+  } catch (e) {
+    console.error(e);
+    res.status(500).send({ status: "error", message: e.message });
+  } finally {
+    await client.close();
+  }
+};
+
+export const updateProject = async (req, res) => {
+  try {
+    await client.connect();
+    const _id =
+      typeof req.body._id === "string"
+        ? new ObjectId(req.body._id)
+        : req.body._id;
+    delete req.body._id;
+    delete req.body.index;
+
+    /** Set editedAt value */
+    req.body.editedAt = dayjs().format();
+
+    const existing = await client
+      .db("waqfeardhi")
+      .collection("projects")
+      .findOne({ _id });
+    const result = await client
+      .db("waqfeardhi")
+      .collection("projects")
+      .updateOne({ _id }, { $set: createUpdatePayload(existing, req.body) });
     res.send({ status: "success", data: result });
   } catch (e) {
     console.error(e);
