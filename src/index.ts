b7a819292f15c839044f489c8ee467d594148fb0
Tofik Khan <tofik.khan@mkausa.org>
2025-06-29 21:41:01 -0700

    Add db test route

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,34 @@
 import express, { Express, Request, Response } from "express";
+import { MongoClient, ObjectId } from "mongodb";
+
+const client = new MongoClient(process.env.DB_CONNECT_STRING);
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+const getFAQs = async (req, res: Response) => {
+  try {
+    await client.connect();
+
+    const result = await client
+      .db("wn-expo")
+      .collection("faqs")
+      .find({})
+      .toArray();
+    res.send({ status: "success", data: result });
+  } catch (e) {
+    console.error(e);
+    res.status(500).send({ status: "error", message: e.message });
+  } finally {
+    await client.close();
+  }
+};
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Ready to run on Heroku.");
 });
+
+app.get("/db", getFAQs);
 
 app.listen(port, () => {
   return console.log(`[server]: Server is running on ${port}`);
