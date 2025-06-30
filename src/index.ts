4a1ed6d04db6a5e9e43824bdf980b484624746e1
Tofik Khan <tofik.khan@mkausa.org>
2025-06-29 21:49:57 -0700

    Update route for comparative testing

--- src/index.ts
+++ src/index.ts
@@ -24,11 +24,30 @@ const getFAQs = async (req, res: Response) => {
   }
 };
 
+const getApplicants = async (req, res: Response) => {
+  try {
+    await client.connect();
+
+    const result = await client
+      .db("waqfeardhi")
+      .collection("applicants")
+      .find({})
+      .toArray();
+    res.send({ status: "success", data: result });
+  } catch (e) {
+    console.error(e);
+    res.status(500).send({ status: "error", message: e.message });
+  } finally {
+    await client.close();
+  }
+};
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Ready to run on Heroku.");
 });
 
-app.get("/db", getFAQs);
+app.get("/waqfeardhi/projects", getFAQs);
+app.get("/waqfeardhi/applicants", getApplicants);
 
 app.listen(port, () => {
   return console.log(`[server]: Server is running on ${port}`);
